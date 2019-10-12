5f124eb39cb349a230f99ee342a00ad141062007
jeck jaimes <jeckjaimes@gmail.com>
2019-10-12 14:15:43 -0400

    Updates

---
+++ precache-manifest.02811d6d646f01f319a1bf54ccf6199d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b276a91b1836da6bee5c6f70d0f232f",
+    "url": "/monsters-react-course/index.html"
+  },
+  {
+    "revision": "583a24c6a3867ce7727c",
+    "url": "/monsters-react-course/static/css/main.b7f373c9.chunk.css"
+  },
+  {
+    "revision": "3e3629346ffa07894a8f",
+    "url": "/monsters-react-course/static/js/2.6b4f4a34.chunk.js"
+  },
+  {
+    "revision": "583a24c6a3867ce7727c",
+    "url": "/monsters-react-course/static/js/main.56bd08ce.chunk.js"
+  },
+  {
+    "revision": "75f31e956eec623b9aa2",
+    "url": "/monsters-react-course/static/js/runtime-main.66deb16e.js"
+  }
+]);
